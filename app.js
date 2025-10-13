dc8bb34819dc3e7c24a8026e9412db16d3ad064d
aya hamed <ayahamedzaki@gmail.com>
2025-10-14 00:52:02 +0300

    update app.js

--- app.js
+++ app.js
@@ -8,25 +8,37 @@ import authRouter from "./src/routes/auth.route.js";
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './docs/swagger.js';
 import searchRoutes from "./src/routes/fullTextSearch.route.js";
-
-
+import bookRouter from "./src/routes/book.route.js";
+import path from 'path';
+import "./src/services/stock.service.js";
+import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 connectDB();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/public/images', express.static(path.join(__dirname, 'public/images')));
 app.use(express.json());
-
-app.use(errorHandler);
-
 // auth routes
 app.use('/api/auth', authRouter);
 
 // #todo check is admin middleware
 app.use('/api/admin/users', userRoutes);
-
+// book routes
+app.use('/api/admin/book',bookRouter)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use((req, res, next) => {
+    return res.status(404).json({
+        status: 'fail',
+        message: 'Route is not found'
+    });
+});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
