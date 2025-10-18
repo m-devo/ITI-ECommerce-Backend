6d35e776a5123fe7da661b8abf27871852b73647
menaemg <abdalmonaem.gamal@gmail.com>
2025-10-18 13:32:02 +0300

    Update increment, decrement and remove items routes to follow Restful API's

--- src/controllers/api/cart/cart.controller.js
+++ src/controllers/api/cart/cart.controller.js
@@ -22,21 +22,21 @@ export const CartController = {
 
     incrementItemQuantity: catchAsync(async (req, res) => {
         const userId = req.currentUser.id;
-        const { bookId } = req.body;
+        const { bookId } = req.params;
         const cart = await CartService.incrementItemQuantity(userId, bookId);
         res.status(200).json(new ApiResponse(200, cart, "Item quantity incremented successfully"));
     }),
 
     decrementItemQuantity: catchAsync(async (req, res) => {
         const userId = req.currentUser.id;
-        const { bookId } = req.body;
+        const { bookId } = req.params;
         const cart = await CartService.decrementItemQuantity(userId, bookId);
         res.status(200).json(new ApiResponse(200, cart, "Item quantity decremented successfully"));
     }),
 
     removeItemFromCart: catchAsync(async (req, res) => {
         const userId = req.currentUser.id;
-        const { bookId } = req.body;
+        const { bookId } = req.params;
         const cart = await CartService.removeItemFromCart(userId, bookId);
         res.status(200).json(new ApiResponse(200, cart, "Item removed from cart successfully"));
     }),
