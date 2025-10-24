981259fdddea4ea5febee2435266d94053acc177
menaemg <abdalmonaem.gamal@gmail.com>
2025-10-24 15:00:16 +0300

    Revert "conflict solved and review and aveRating and doc added"
    This reverts commit 5ab107475661aa003c5e1295453d3b6847708ef4.

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -4,11 +4,11 @@ const cartSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        unique: true,
+        unique: true, 
         required: true
     },
-    items: [{
-        bookId: {
+    items: [{ 
+        bookId: { 
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Book',
             required: true
@@ -19,18 +19,22 @@ const cartSchema = new mongoose.Schema({
             min: [1, 'Quantity can not be less than 1.'],
             default: 1
         }
-    }]
+    }],
+    reminderSent: {
+        type: Boolean,
+        default:false
+    }
 }, {
     timestamps: true
-})
+});
 
-cartSchema.pre("save", function (next) {
+cartSchema.index({ reminderSent: 1, updatedAt: 1 });
+
+cartSchema.pre("save", function(next) {
     if (this.isModified("items")) {
-        this.lastUpdateAt = new Date();
-        this.reminderSent = false
+        this.reminderSent = false;
     }
-    next()
-
-})
+    next();
+});
 
 export const Cart = mongoose.model("Cart", cartSchema);
